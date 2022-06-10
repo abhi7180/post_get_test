5fd1a71184d5ad173a9236d067d4751ba8c28a5e
vadiyagnik <vadiyagnik@gmail.com>
2022-06-10 09:51:25 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,18 +20,41 @@ class first extends StatefulWidget {
 class _firstState extends State<first> {
 
   List<mydata> list =[];
+  mydata? m;
+  bool isLoading = true;
+
   get()
   async {
     var url = Uri.parse('https://abhiflutter.000webhostapp.com/test.php');
     var response = await http.post(url);
     print('Response status: ${response.statusCode}');
-    // print('Response body: ${response.body}');
+    print('Response body: ${response.body}');
 
-    dynamic result = jsonDecode(response.body);
+    Map<String,dynamic> result = jsonDecode(response.body);
 
-    result.forEach((v){
-      list.add(mydata.fromJson(v));
+    if (response.statusCode == 200) {
+      m = await mydata.fromJson(result);
+      if (m != null) {
+        setState(() {
+          isLoading = false;
+        });
+      } else {
+        print("null1");
+      }
+    } else {
+      print("null2");
+    }
+
+    setState(() {
+      m = mydata.fromJson(result);
     });
+
+
+
+    // result.forEach((k,v){
+    //   list.add(mydata.fromJson(v));
+    // });
+
 
   }
 
@@ -46,14 +69,38 @@ class _firstState extends State<first> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView.builder(
-        itemCount: list.length,
-        itemBuilder: (context, index) {
-        return ListTile(
-          title: Text("${list[index].data}"),
+      body: isLoading ?  Center(
+        child: CircularProgressIndicator(),
+       ) :  ListView.separated(itemBuilder: (context, index) {
 
-        );
-      },),
+        return Column(
+                  children: [
+                    ListTile(
+                      leading: Text("${m!.data![index].id}"),
+                      title: Text("${m!.data![index].address}"),
+                      subtitle: Text("${m!.data![index].addressType}"),
+
+
+                    ),
+                    ListTile(
+                      leading: Text("${m!.data![index].lat}"),
+                      title: Text("${m!.data![index].lang}"),
+                      subtitle: Text("${m!.data![index].landmark}"),
+
+                    ),
+                    ListTile(
+                      leading: Text("${m!.data![index].building}"),
+                      title: Text("${m!.data![index].pincode}"),
+                      subtitle: Text("${m!.data![index].deliveryCharge}"),
+                    ),
+
+
+                  ],
+                );
+       }, separatorBuilder: (context,index){
+         return Container(height: 2,color: Colors.black,);
+      }, itemCount: m!.data!.length)
+
     );
   }
 }
